35e3d103efa1910d16ac19816086ddbaff9715e9
okjason-source <okjason-source@users.noreply.github.com>
2026-03-06 11:20:57 +0000

    deploy: c902d311d04c4d24af231ccc2861f0cbacc6ac3a

--- trait.impl/clap_builder/derive/trait.ValueEnum.js
+++ trait.impl/clap_builder/derive/trait.ValueEnum.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["dist_agent_lang",[["impl ValueEnum for <a class=\"enum\" href=\"dist_agent_lang/cli/enum.ColorChoice.html\" title=\"enum dist_agent_lang::cli::ColorChoice\">ColorChoice</a>"]]]]);
+    const implementors = Object.fromEntries([["dist_agent_lang",[["impl ValueEnum for <a class=\"enum\" href=\"dist_agent_lang/cli/enum.ColorChoice.html\" title=\"enum dist_agent_lang::cli::ColorChoice\">ColorChoice</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[178]}
+//{"start":59,"fragment_lengths":[180]}
--- trait.impl/core/cmp/trait.PartialOrd.js
+++ trait.impl/core/cmp/trait.PartialOrd.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["dist_agent_lang",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.1/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"enum\" href=\"dist_agent_lang/performance/concurrency/enum.TaskPriority.html\" title=\"enum dist_agent_lang::performance::concurrency::TaskPriority\">TaskPriority</a>"]]]]);
+    const implementors = Object.fromEntries([["dist_agent_lang",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"enum\" href=\"dist_agent_lang/performance/concurrency/enum.TaskPriority.html\" title=\"enum dist_agent_lang::performance::concurrency::TaskPriority\">TaskPriority</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[357]}
+//{"start":59,"fragment_lengths":[359]}
