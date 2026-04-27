d820bfc9ee65252d20634581f08748b62cec46fc
okjason-source <okjason-source@users.noreply.github.com>
2026-04-27 15:29:07 +0000

    deploy: e762f8ca7e44589a7d18e4d821417df02ea038a0

--- api/trait.impl/core/cmp/trait.PartialOrd.js
+++ api/trait.impl/core/cmp/trait.PartialOrd.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["dist_agent_lang",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"enum\" href=\"dist_agent_lang/performance/concurrency/enum.TaskPriority.html\" title=\"enum dist_agent_lang::performance::concurrency::TaskPriority\">TaskPriority</a>",0]]]]);
+    const implementors = Object.fromEntries([["dist_agent_lang",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"enum\" href=\"dist_agent_lang/performance/concurrency/enum.TaskPriority.html\" title=\"enum dist_agent_lang::performance::concurrency::TaskPriority\">TaskPriority</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
